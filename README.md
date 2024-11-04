27e8787a9ceba5e24023c9e451da5cd31cdee0b3
VictorGakiria <vmark119@gmail.com>
2024-11-04 23:08:26 +0300

    ll

---
+++ README.md
@@ -0,0 +1,19 @@
+# .REACT-COURSE-
+
+open-source front-end JavaScript library for building user interfaces based on components.
+
+--CDN LINK--- MUST BE INCLUDED
+
+<script crossorigin src="https://unpkg.com/react@18/umd/react.development.js"></script>
+<script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script
+  
+  
+  at the source , for script...this is added!
+<script src="C:\Users\Mak Mende!\Desktop\PROJES!!\.REACT\react.js" type="text/babel"> added also
+  
+  
+  at the script tag
+
+
+
+allow access to file URL's...in the chrome extension 
---
+++ react.js
@@ -0,0 +1,36 @@
+ import React from "react"
+
+ import ReactDOM from "ReactDOM"
+
+const a=<p>Test!</p>
+
+ReactDOM.render(a,document.getElementById('root'))
+
+
+
+ // jsx -- a flavour to js, enables html script inside js
+
+ // className. To specify a CSS class, use the className attribute. This applies to all regular DOM and SVG elements like <div> , <a> , and others. If you use 
+ //React with Web Components (which is uncommon), use the class attribute instead.
+
+
+/*
+
+ const page=
+ <nav>
+    <h1>SIMPLE NAVBAR</h1>
+ <ul>
+    <li><a>Pricing</a></li>
+    <li><a>About</a></li>
+    <li><a>Contact</a></li>
+ </ul>
+ </nav>
+
+ ReactDOM.render(page ,document.getElementById("root"))
+
+*/
+
+
+
+
+
